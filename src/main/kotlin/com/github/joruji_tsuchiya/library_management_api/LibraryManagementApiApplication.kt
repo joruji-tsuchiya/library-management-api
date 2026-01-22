74941de686af12910d057528195971c56802240c
joruji-tsuchiya <tuguxuliuzhi@gmail.com>
2026-01-22 19:39:02 +0900

    chore: Spring Bootプロジェクトの初期化と依存関係の追加

---
+++ src/main/kotlin/com/github/joruji_tsuchiya/library_management_api/LibraryManagementApiApplication.kt
@@ -0,0 +1,11 @@
+package com.github.joruji_tsuchiya.library_management_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryManagementApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibraryManagementApiApplication>(*args)
+}
